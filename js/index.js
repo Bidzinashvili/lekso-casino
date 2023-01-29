6e89dc5746dbe667c049cfb751b799ab45ddc22f
Lasha Bidzinashvili <116507932+Bidzinashvili@users.noreply.github.com>
2023-01-29 19:27:34 +0400

    .

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  document.body.classList.toggle("active");
+});
+
+const modal = document.querySelector(".modal");
+const modalCloser = document.querySelector("[data-modal-close]");
+
+const openModal = () => {
+  modal.classList.add("active");
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.classList.remove("active");
+  document.body.style.overflow = "visible";
+};
+
+modalCloser.addEventListener("click", closeModal);
+
+const buttons = document.querySelectorAll(".card--button");
+buttons.forEach((button) => {
+  button.addEventListener("click", openModal);
+});
